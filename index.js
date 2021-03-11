c859e83631a650ccc617e17fc053c5a206ab1aea
AlinMiclaus <miclaus1804@gmail.com>
2021-03-11 21:00:37 +0200

    my error course

--- index.js
+++ index.js
@@ -1,29 +1,116 @@
-console.log("course 12");
+console.log("Course 16 - ECMA Script");
 
-console.log("JavaScript - Async Programming");
+console.log(a);
+var a = 6;
+a = 7;
+console.log(a);
 
-document.addEventListener("click", onClick);
+// console.log(b);
+let b ;
+console.log(b);
+b = 7;
+console.log(b);
 
-let counter = 0;
-function onClick() {
-  console.log("just a click");
-  counter++;
-  console.log("counter:", counter);
-}
-console.log("counter:", counter);
 
-// the function is an anonymous function
-setTimeout(function () {
-  console.log("This is a code ran after 2 seconds");
-}, 2000);
-
-// this is similar to
-setTimeout(setTimeoutFunction, 2000);
-
-function setTimeoutFunction() {
-  console.log("This is a code ran after 2 seconds");
+let c = "a sentence";
+console.log(c);
+if (true) {
+  let c = "a word";
+  console.log(c);
 }
 
-setInterval(function () {
-  console.log("ping");
-}, 1000);
+console.log(c);
+
+var x = "a sentence"
+console.log(x);
+if (true) {
+  var x = "a word";
+  console.log(x);
+}
+
+console.log(x);
+
+var y = 1;
+function increment(){
+  var y = 2;
+  y++
+  console.log(y);
+  }
+
+  increment();
+  console.log(y);
+
+  // function variation
+
+  function nameFunction (a,b) {
+    //instructions
+    return;
+  };
+
+  // anonymous functions <=> expression functions
+
+  let anonymous = function() {
+    console.log("This is a anonymous");
+  }
+
+  anonymous();
+
+// ES6 arrow functions (is an anonymous function)
+// no parameters!!!
+const noParams = () => {
+  console.log("This is arrow, no parameters!");
+  return;
+}
+noParams();
+
+//with parameters
+const withParameters = (a,b) => {
+  return a < b;
+}
+const resultWithParameters = withParameters(3,4);
+console.log(resultWithParameters);
+
+// no curly braces
+const noCurly = () => 1 + 1;
+const resultCurly = noCurly();
+console.log(resultCurly);
+
+// no parentheses - when you have a single parameter
+const noParentheses = a => a + 1;
+const resultNoParentheses = noParentheses(5);
+console.log(resultNoParentheses); 
+
+const array = [2,6,3,1,9,6];
+const arrayEven = array.filter(element => element % 2 ? false : true )
+console.log(arrayEven);
+
+// old style - ES 5
+
+const arrayEvenES5 = array.filter(function(element) {
+  return element % 2 ? false : true;
+});
+console.log(arrayEvenES5);
+
+const arrayOdd = array.filter(element => element % 2)
+console.log(arrayOdd);
+
+
+function ourFilter (originalArray, filterFunction) {
+  const filteredArray = [];
+  for (const element of originalArray) {
+    if (filterFunction (element)) {
+      console.log("element passed condition", element);
+      filteredArray.push(element);
+    }
+  }
+
+  return filteredArray;
+}
+
+function isUneven () {
+  console.log(element % 2);
+  return element % 2;
+}
+
+const resultOurUnevenArray = ourFilter(array, isUneven);
+console.log(resultOurUnevenArray);
