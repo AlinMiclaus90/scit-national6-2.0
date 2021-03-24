8fd96803ca4d45eae979a62011f012eb364861ff
AlinMiclaus <miclaus1804@gmail.com>
2021-03-24 10:20:55 +0200

    working game, course 18

--- game.js
+++ game.js
@@ -7,34 +7,13 @@ class GameObject {
     this.x = 0;
     this.y = 0;
     this.generateRef();
+    // this.move(50, 225);
   }
 
   generateRef() {
     this.ref = document.createElement("div");
     this.ref.style.width = `${this.width}px`;
     this.ref.style.height = `${this.height}px`;
-    this.ref.style.position = "absolute";
-    this.ref.style.top = 0;
-    this.ref.style.left = 0;
-
-    document.getElementById("game-scene").appendChild(this.ref);
-  }
-}
-
-class Player extends GameObject {
-  constructor() {
-    super();
-    this.ref.style.background = "red";
-    this.move(50, 225);
-  }
-
-  
-
-  generateRef() {
-    this.ref = document.createElement("div");
-    this.ref.style.width = `${this.width}px`;
-    this.ref.style.height = `${this.height}px`;
-    this.ref.style.background = "blue";
     this.ref.style.position = "absolute";
     this.ref.style.top = 0;
     this.ref.style.left = 0;
@@ -47,6 +26,14 @@ class Player extends GameObject {
     this.y = y;
     this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
   }
+}
+
+class Player extends GameObject {
+  constructor() {
+    super();
+    this.ref.style.background = "blue";
+    this.move(50, 225);
+  }
 
   moveUp() {
     this.move(this.x, this.y - 25);
@@ -56,6 +43,20 @@ class Player extends GameObject {
     this.move(this.x, this.y + 25);
   }
 }
+
+class Obstacle extends GameObject {
+  constructor() {
+    super();
+    this.ref.style.background = "red";
+    this.move(1060, 25);
+  }
+
+  moveLeft() {
+    this.move(this.x - 5, this.y);
+  }
+}
+
+/// --- User  input
 
 let keyUpPress = false;
 let keyDownPress = false;
@@ -79,19 +80,7 @@ document.addEventListener("keyup", (event) => {
   }
 });
 
-class Obstacle extends GameObject {
-    constructor() {
-        super() 
-        this.ref.style.background = "red";
-        this.move(1060, 25);
-    }
-
-    moveLeft() {
-        this.move(this.x - 5, this.y)
-    }
-}
-
-// user input
+/// --- User  input
 
 const player = new Player();
 const obstacle = new Obstacle();
