8ed793e9dd77d0df113f01579ce01cfcdfc12962
AlinMiclaus <miclaus1804@gmail.com>
2021-03-29 15:10:29 +0300

    final homework

--- index.js
+++ index.js
@@ -1,68 +1,170 @@
-console.log("My beautiful Homework");
+console.log('to-do-app-homework-amiclaus')
+
+const taskListHtml = document.querySelector(".added-task");
+const trashSvg =
+  '<svg height="30px" viewBox="-40 0 427 427.00131" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="m232.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0"/><path d="m114.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0"/><path d="m28.398438 127.121094v246.378906c0 14.5625 5.339843 28.238281 14.667968 38.050781 9.285156 9.839844 22.207032 15.425781 35.730469 15.449219h189.203125c13.527344-.023438 26.449219-5.609375 35.730469-15.449219 9.328125-9.8125 14.667969-23.488281 14.667969-38.050781v-246.378906c18.542968-4.921875 30.558593-22.835938 28.078124-41.863282-2.484374-19.023437-18.691406-33.253906-37.878906-33.257812h-51.199218v-12.5c.058593-10.511719-4.097657-20.605469-11.539063-28.03125-7.441406-7.421875-17.550781-11.5546875-28.0625-11.46875h-88.796875c-10.511719-.0859375-20.621094 4.046875-28.0625 11.46875-7.441406 7.425781-11.597656 17.519531-11.539062 28.03125v12.5h-51.199219c-19.1875.003906-35.394531 14.234375-37.878907 33.257812-2.480468 19.027344 9.535157 36.941407 28.078126 41.863282zm239.601562 279.878906h-189.203125c-17.097656 0-30.398437-14.6875-30.398437-33.5v-245.5h250v245.5c0 18.8125-13.300782 33.5-30.398438 33.5zm-158.601562-367.5c-.066407-5.207031 1.980468-10.21875 5.675781-13.894531 3.691406-3.675781 8.714843-5.695313 13.925781-5.605469h88.796875c5.210937-.089844 10.234375 1.929688 13.925781 5.605469 3.695313 3.671875 5.742188 8.6875 5.675782 13.894531v12.5h-128zm-71.199219 32.5h270.398437c9.941406 0 18 8.058594 18 18s-8.058594 18-18 18h-270.398437c-9.941407 0-18-8.058594-18-18s8.058593-18 18-18zm0 0"/><path d="m173.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0"/></svg>';
 
 
-document
-.getElementById("add-task-button")
-.addEventListener("click", () => {
-    console.log("It's working!");
-    const userInput = document.getElementById("input").value;    
-    console.log("User Input is:", userInput);
 
-    // if (userInput) {
-        const payload = {
-            id: "amiclaus",
-            todo:[
-              {
-                checked: false,
-                item: userInput
-              },
-           ]
-        }
-    // }
-
-    console.log("Payload object:", payload)
-    console.log("Payload string:", JSON.stringify(payload));
-
-    fetch('https://simple-json-server-scit.herokuapp.com/todo', {
-  method: "POST",
-  headers: {
-      'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(payload)
-})
-.then(getData);
-});
+document.getElementById("add-task-button").addEventListener("click", getData);
 
 function getData() {
-  document
-  .getElementById("add-task-button")
-  .addEventListener("click", () => {
-    fetch('https://simple-json-server-scit.herokuapp.com/todo')
-    .then(handleFetchResponse)
-    .then(useJSONResponse);
+  const addedTask = document.querySelector(".input-task").value;
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/amiclaus")
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+      if (data.todo) {
+        return putData(addedTask, data);
+      } else {
+        return postData(addedTask);
+      }
+    })
+    .then(() => renderToDoList());
+}
+
+function postData(addedTask) {
+  console.log(addedTask);
+  if (addedTask) {
+    const payload = {
+      id: "amiclaus",
+      todo: [
+        {
+          checked: false,
+          item: addedTask
+        }
+      ],
+    }
+
+    return fetch('https://simple-json-server-scit.herokuapp.com/todo', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+  }
+
+}
+
+function putData(addedTask, data) {
+  if (addedTask) {
+    const newTask = {
+      checked: false,
+      item: addedTask
+    };
+    data.todo.push(newTask);
+    return fetch("https://simple-json-server-scit.herokuapp.com/todo/amiclaus", {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  }
+}
+
+
+function renderToDoList() {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/amiclaus")
+    .then(r => r.json())
+    .then((data) => {
+      console.log(data);
+      taskListHtml.innerHTML = "";
+      if (data.todo) {
+        for (const taskItem of data.todo) {
+          renderToDoTask(taskItem, data);
+        }
+      }
+    })
+
+}
+
+function renderToDoTask(taskItem, data) {
+  console.log(taskItem);
+  const task = document.createElement("div");
+  task.classList.add("taskStyle");
+  const checkedLi = document.createElement("input");
+  checkedLi.setAttribute("type", "checkbox");
+
+  const taskContent = document.createElement("p");
+  const removeArticle = document.createElement("div");
+  removeArticle.innerHTML = trashSvg;
+
+  task.appendChild(checkedLi);
+  task.appendChild(taskContent);
+  task.appendChild(removeArticle);
+  taskListHtml.appendChild(task);
+
+  taskContent.innerText = taskItem.item;
+
+  removeArticle.addEventListener("click", function () {
+    task.remove();
+    deleteData(taskItem.item, data);
+
+  });
+  checkedLi.addEventListener("click", function() {
+    if (checkedLi.checked === true) {
+      taskItem.checked = true;
+      return fetch("https://simple-json-server-scit.herokuapp.com/todo/amiclaus", {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    } else if (checkedLi.checked === false){
+      taskItem.checked = false;
+      return fetch("https://simple-json-server-scit.herokuapp.com/todo/amiclaus", {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    }
   })
-};
+ }
 
-function handleFetchResponse(response) {
-  console.log("response is:", response);
-  return response.JSON();
+function deleteData(item, data) {
+
+  const index = data.todo.findIndex((task) => {
+    console.log(task.item, item);
+    return task.item === item
+  });
+  console.log(index);
+  data.todo.splice(index, 1);
+
+  fetch(
+    "https://simple-json-server-scit.herokuapp.com/todo/amiclaus",
+    {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+  )
 }
 
-function useJSONResponse(json) {
-  console.log("JSON is:", json);
-  renderTasks(json);
+document.addEventListener("DOMContentLoaded", onContentLoad);
+
+function onContentLoad() {
+  fetchData()
+    .then(data => renderContent(data));
 }
 
-function renderTasks(tasks) {
-  const taskList = document.getElementsByClassName("task-list");
+function fetchData() {
+  return fetch("https://simple-json-server-scit.herokuapp.com/todo/amiclaus")
 
-  const taskItem = document.createElement("div");
+    .then((r) => r.json())
+    .then((data) => data);
+}
 
+function renderContent(data) {
 
-  taskList.appendChild(taskItem);
-
-  taskItem.innerText = taskItem;
-
-
-
+  data.todo.forEach(data => {
+    console.log(data);
+    renderToDoList(data);
+  });
 }
