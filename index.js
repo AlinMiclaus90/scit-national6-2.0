358c7684da6ef3372264905eebc7b09d13c68c1a
AlinMiclaus <miclaus1804@gmail.com>
2021-03-30 19:07:00 +0300

    PROMISES

---
+++ index.js
@@ -0,0 +1,55 @@
+// console.log("RECAP");
+
+// document.addEventListener("click", () => {
+//   console.log("click");
+// });
+
+// setTimeout(() => {
+//   console.log("time end");
+// }, 1000);
+
+// const interval = setInterval(() => {
+//   console.log("ping");
+// }, 2000);
+
+// clearInterval(interval);
+
+
+// we need to call "secondStep" only after "firstStep" ended
+
+// function firstStep() {
+//     setTimeout(() => {
+//         console.log("End of first step!");
+//         secondStep();
+//     }, 2000);
+    
+// };
+
+// function secondStep() {
+//     setTimeout(() => {
+//         console.log("End of second step")
+//         thirdStep();
+//     }, 1000);
+// };
+
+// function thirdStep() {
+//     setTimeout(() => {
+//         console.log("End of third step")
+//     }, 500);
+// };
+
+// firstStep();
+
+
+// Promises
+
+function firstStep() {
+return new Promise()
+
+
+    setTimeout(() => {
+        console.log("End of first step!");
+        secondStep();
+    }, 2000);
+    
+};
