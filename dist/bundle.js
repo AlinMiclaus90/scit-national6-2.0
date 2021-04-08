cb1f24e0acf098424662554240d6576c5e87a047
AlinMiclaus <miclaus1804@gmail.com>
2021-04-08 21:03:48 +0300

    add html & css

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("Entry mode in WebpPack");const o=Math.floor(20*Math.random());console.log(o);const n=`${o}px`;console.log(n);const c=`${o}%`;console.log(c)})();
+console.log("To do app");
