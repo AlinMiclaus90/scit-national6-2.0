ceb6ea67c02be6741120158c00484fe6b3d1d757
AlinMiclaus <miclaus1804@gmail.com>
2021-04-13 10:22:23 +0300

    final homework

--- index.js
+++ index.js
@@ -1,72 +1,68 @@
 console.log("spaceship-homework-amiclaus");
 
-
+let newSpaceship;
 document
   .getElementById("generate-ship-button")
   .addEventListener("click", () => {
     console.log("Generate Car!");
-    const newSpaceship = new Spaceship();
+    newSpaceship = new Spaceship();
   });
-
-
 
 class Spaceship {
   constructor() {
-      this.runningEngine = false;
-    this.position - 0;
+    this.runningEngine = false;
+    this.x = 0;
+    this.y = 0;
     this.generateHTMLRef();
-    this.setStartEngine();
+    // this.setStartEngine();
     this.setMoveSpaceship();
   }
 
- generateHTMLRef() {
-      this.ref = document.createElement("img");
-      let x = Math.floor(Math.random() * 3);
-      console.log(x);
-      this.ref.src = `spaceship${x}.svg`;
-      this.ref.classList.add("spaceship");
-      document.body.appendChild(this.ref);
+  generateHTMLRef() {
+    this.ref = document.createElement("img");
+    let x = Math.floor(Math.random() * 3);
+    console.log(x);
+    this.ref.src = `spaceship${x}.svg`;
+    this.ref.classList.add("spaceship");
+    document.body.appendChild(this.ref);
   }
 
-  setStartEngine() {
-      this.ref.addEventListener("click", () => {
-          this.startEngine();
-      })
-  }
+  // setStartEngine() {
+  //   this.ref.addEventListener("click", () => {
+  //     this.startEngine();
+  //   });
+  // }
 
-  startEngine() {
-      if (!this.runningEngine) {
-          this.runningEngine = true;
-          console.log("Engine is start!");
-          this.ref.classList.add("engine--start");
-      }
-  }
+  // startEngine() {
+  //   if (!this.runningEngine) {
+  //     this.runningEngine = true;
+  //     console.log("Engine is start!");
+  //     this.ref.classList.add("engine--start");
+  //   }
+  // }
 
   setMoveSpaceship() {
-      document.addEventListener("keydown", (event) => {
-        console.log(event);
-        if (this.runningEngine) {
-            this.moveSpaceship(event.key);
-        }
-      })
+    document.addEventListener("keydown", (event) => {
+      console.log(event);
+      // if (this.runningEngine) {
+      newSpaceship.moveSpaceship(event.key);
+      // }
+    });
   }
 
   moveSpaceship(direction) {
     if (direction === "ArrowRight") {
-        this.position += 10;
-        this.ref.style.marginLeft = `${this.position}px`
+      this.x += 5;
+      this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
     } else if (direction === "ArrowLeft") {
-        this.position -= 10;
-        this.ref.style.marginLeft = `${this.position}px`
+      this.x -= 5;
+      this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
     } else if (direction === "ArrowDown") {
-        this.position += 10;
-        this.ref.style.marginTop = `${this.position}px`
+      this.y += 5;
+      this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
     } else if (direction === "ArrowUp") {
-        this.position -= 10;
-        this.ref.style.marginTop = `${this.position}px`
-    };
+      this.y -= 5;
+      this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
+    }
   }
-
-
-
 }
