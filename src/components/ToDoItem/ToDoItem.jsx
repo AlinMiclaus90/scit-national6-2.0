0be207f3e0a99be960cc7fa338fd4ca5c429bfa8
AlinMiclaus <miclaus1804@gmail.com>
2021-04-22 20:57:35 +0300

    add and remove item

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,7 +1,7 @@
 import "./ToDoItem.css";
 
 export function ToDoItem(props) {
-  console.log(props);
+  // console.log(props);
 
   return (
     <div className="to-do-item">
@@ -10,6 +10,7 @@ export function ToDoItem(props) {
       <img
         src="https://www.flaticon.com/svg/vstatic/svg/860/860829.svg?token=exp=1618938202~hmac=5372da9d227dec8ad0c250a251d064ab"
         alt="trash can"
+        onClick={(event) => props.removeItem(props.label)}
       />
     </div>
   );
