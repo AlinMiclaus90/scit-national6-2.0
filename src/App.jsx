294a8af0868abd33e0f91b3bf3cea65a4887a238
AlinMiclaus <miclaus1804@gmail.com>
2021-04-29 18:33:29 +0300

    stef commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header/Header";
+import { ToDoList } from "./components/ToDoList/ToDoList";
+
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    showToDoList: true,
+  };
+
+  toggleToDoListVisibility = () => {
+    this.setState({ showToDoList: !this.state.showToDoList });
+  };
+
+  render() {
+    return (
+      <div className="App" id="app">
+        <Header />
+        <button onClick={this.toggleToDoListVisibility}>
+          Hide/Show ToDoList
+        </button>
+        {this.state.showToDoList ? <ToDoList /> : null}
+      </div>
+    );
+  }
+}
+
+export default App;
